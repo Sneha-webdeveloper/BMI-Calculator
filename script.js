0a684c6726ca755c4444e8cfaabd678fab608004
Sneha-webdeveloper <2001snehas@gmail.com>
2023-12-13 01:40:35 +0530

    new features addedd

--- script.js
+++ script.js
@@ -12,18 +12,26 @@ function calculateBMI() {
         const bmi = Math.ceil(weight / ((height / 100) ** 2));
 
         if (bmi < 18.5) {
-            result.innerHTML = `<h2 class="text-center">${bmi} kg/m²</h2><h4 class="text-center text-info">Underweight</h4><p class="my-3">Focus on nutrient-dense foods to gain weight gradually. Regular exercise can help build muscle mass.</p>`;
+            result.innerHTML = `<h2 class="text-center">${bmi} kg/m²</h2><h4 class="text-center text-info fw-bolder">Underweight</h4><p class="my-3">Focus on nutrient-dense foods to gain weight gradually. Regular exercise can help build muscle mass.</p>`;
+            document.getElementById("arrow").style.transform="rotate(-191deg)"
         } else if (bmi < 25) {
-            result.innerHTML = `<h2 class="text-center">${bmi} kg/m²</h2><h4 class="text-center text-success">Healthy</h4><p class="my-3">Maintain a balanced diet and regular physical activity to sustain overall health.</p>`;
-        } else if (bmi < 30) {
-            result.innerHTML = `<h2 class="text-center">${bmi} kg/m²</h2><h4 class="text-center text-warning">Overweight</h4><p class="my-3">Adopt healthy eating habits, increase physical activity, and consider consulting a healthcare professional for guidance.</p>`;
-        } else {
-            result.innerHTML = `<h2 class="text-center">${bmi} kg/m²</h2><h4 class="text-center text-danger">Obese</h4><p class="my-3">Focus on gradual weight loss through a combination of diet changes, increased exercise, and professional support.</p>`;
-        }
-    }
+            result.innerHTML = `<h2 class="text-center">${bmi} kg/m²</h2><h4 class="text-center text-success fw-bolder">Healthy</h4><p class="my-3">Maintain a balanced diet and regular physical activity to sustain overall health.</p>`;
+            document.getElementById("arrow").style.transform="rotate(-129deg)"
 
-   
-   
+        } else if (bmi < 30) {
+            result.innerHTML = `<h2 class="text-center">${bmi} kg/m²</h2><h4 class="text-center text-warning fw-bolder">Overweight</h4><p class="my-3">Adopt healthy eating habits, increase physical activity, and consider consulting a healthcare professional for guidance.</p>`;
+            document.getElementById("arrow").style.transform="rotate(-68deg)"
+
+        } else {
+            result.innerHTML = `<h2 class="text-center">${bmi} kg/m²</h2><h4 class="text-center text-danger fw-bolder">Obese</h4><p class="my-3">Focus on gradual weight loss through a combination of diet changes, increased exercise, and professional support.</p>`;
+            document.getElementById("arrow").style.transform="rotate(4deg)"
+
+        }
+        const speech= new SpeechSynthesisUtterance(` your bmi is ${bmi}`);
+        speechSynthesis.speak(speech);
+    }
+  
+
 }
 
 function allclear(){
